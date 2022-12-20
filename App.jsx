a8e114270940dbbe6254d9c58f029dc33b88909c
VANI_SHA <106505549+vanishasaini@users.noreply.github.com>
2022-12-20 12:22:01 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Product from "./components/product";
+
+
+
+const App = () => {
+  return <Home/>
+          
+          
+          
+          ;
+};
+
+export default App;
---
+++ data.js
@@ -0,0 +1,87 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://digi10gindia.com/website/img/about-bg-01.png",
+      title: "SUPER SALE",
+      desc: "IT'S TIME TO SHOP! AMAZING DEALS YOU HAVE NEVER WITNESS BEFORE.",
+      bg: "fcf1ed",  
+    },
+    {
+      id: 2,
+      img: "https://st3.depositphotos.com/13335314/15843/i/950/depositphotos_158438318-stock-photo-young-family-shopping-together.jpg1",
+      title: "DISCOUNT OVER 50%",
+      desc: "IT'S TIME TO SHOP! AMAZING DEALS YOU HAVE NEVER WITNESS BEFORE.",
+      bg: "f5fafd",
+    },
+    {
+      id: 3,
+      img: "https://thumbs.dreamstime.com/b/super-sale-vector-creative-banner-illustration-abstract-concept-discount-promotion-layout-white-background-sticker-super-sale-125736458.jpg",
+      title: "DEAL OF THE DAY !",
+      desc: "IT'S TIME TO SHOP! AMAZING DEALS YOU HAVE NEVER WITNESS BEFORE..",
+      bg: "ff5fafd",
+    },
+  ];
+ 
+  
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://blog.nihaojewelry.com/wp-content/uploads/2020/04/puffy-sleeves%EF%BB%BF.jpg",
+      title: "WOMAN WEAR",
+      
+    },
+    {
+      id: 2,
+      img: "https://th.bing.com/th/id/OIP.xbJG9TQMRzlbVIUFlmjjbAHaLF?pid=ImgDet&rs=1",
+      title: "MAN WEAR",
+    },
+    {
+      id: 3,
+      img: "https://i.pinimg.com/originals/12/a9/e5/12a9e56e68cdff4a5ea0d2a1b4a4be2b.jpg",
+      title: "KIDS WEAR",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://i5.walmartimages.com/asr/62172a9a-189d-4c1e-8dba-538408b2ee29_1.a53b0fb57dbe11191c029d0b201f5baf.jpeg",
+      
+    },
+    {
+      id:2,
+      img:"https://th.bing.com/th/id/OIP.qJBLQe_3wcBiqYho4TPJrAHaHa?pid=ImgDet&w=178&h=178&c=7&dpr=1.5",
+     
+    },
+    {
+      id:3,
+      img:"https://th.bing.com/th/id/OIP.YpvBFWved30MPjftLa9-hQHaHa?pid=ImgDet&rs=1",
+     
+    },
+    {
+      id:4,
+      img:"https://th.bing.com/th/id/OIP.399cltN8hVk76ZIdCKbTRQHaKX?pid=ImgDet&rs=1",
+     
+    },
+    {
+      id:5,
+      img:"https://th.bing.com/th/id/OIP.UEjBtRZIpRC6tHLfulJ4dQHaLH?pid=ImgDet&w=1240&h=1860&rs=1",
+      title: "KIDS WEAR",
+    },
+    {
+      id:6,
+      img:"https://i5.walmartimages.com/asr/70d49a26-d69a-461b-9189-c0018c2b1f85_1.4854edbb8ee1589a932290770cba427b.jpeg",
+      title: "KIDS WEAR",
+    },
+    {
+      id:7,
+      img:"https://www.little-mistress.com/images/products/zoom/1426773360-16237400.jpg",
+      title: "KIDS WEAR",
+    },
+    {
+      id:8,
+      img:"https://th.bing.com/th/id/OIP.CJHNC67txtF1lGzD8XTMHAHaHa?pid=ImgDet&rs=1",
+      title: "KIDS WEAR",
+    },
+  ]
